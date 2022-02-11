db3a6c51bd4065d3bafde09be284de9672b43228
kepin7 <hizkiakevin8@gmail.com>
2022-02-11 22:40:33 +0700

    modul 4

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -1,23 +1,26 @@
 @extends('layouts.main')
 
 @section('container')
+
+
 <div class="title-top mb-5">
-    <h2 class"fw-bold">Contacts Us</h2>
+    <h2 class="fw-bold">Contact Us</h2>
 </div>
-<form>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
-  </div>
-  <div class="mb-3 form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
+<form method="post" action="{{route('contacts.store')}}">
+    {{csrf_field()}}
+    <div class="form-floating mb-3">
+        <input type="text" class="form-control" id="inputname" placeholder="Nama" name="nama">
+        <label for="inputname">Nama</label>
+    </div>
+    <div class="form-floating mb-3">
+        <input type="Email" class="form-control" id="inputemail" placeholder="Email" name="email">
+        <label for="inputemail">Email</label>
+    </div>
+    <div class="form-floating mb-3">
+        <textarea class="form-control" id="floatingtextarea2" placeholder="Tinggalkan Pesan disini" style="height: 100px" name="pesan"></textarea> 
+        <label for="floatingtextarea2">Pesan</label>
+    </div>
+  <button type="submit" class="btn btn-primary">Kirim</button>
 </form>
+
 @endsection
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -16,7 +16,7 @@
           <a class="nav-link {{ ( $title === 'Gallery') ? 'active' : '' }} "href="/gallery">Gallery</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link {{ ( $title === 'Contacts') ? 'active' : '' }}" href="/contacts">Countact Us</a>
+          <a class="nav-link {{ ( $title === 'Contacts') ? 'active' : '' }}" href="/contacts">Contact Us</a>
         </li>
       </ul>
     </div>
